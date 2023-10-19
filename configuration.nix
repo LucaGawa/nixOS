26b0e4ae4f9b6eac7bf6c26c485f2543c684284e
Luca Gawalleck <luca.gawalleck@gmail.com>
2023-10-19 18:17:58 +0200

    Desktop version without nvidia

--- configuration.nix
+++ configuration.nix
@@ -64,7 +64,7 @@ in
 
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
      # <home-manager/nixos>
     ];
 
@@ -73,7 +73,7 @@ in
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
-  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiSupport = false;
 
   networking.hostName = "nixos"; # Define your hostname.
 
@@ -206,56 +206,8 @@ in
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
-
-#wayland.windowManager.hyprland.enableNvidiaPatches = true;
-# Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-	vulkan-validation-layers
-    ];
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia" "intel"];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = true;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-    open = true;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-    # perhabs solve tearing issue
-    #forceFullCompositionPipeline = true;
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-
-    prime = {
-    	#sync.enable = true;
-	intelBusId = "PCI:0:2:0";
-	nvidiaBusId = "PCI:1:0:0";
-    };
-  };
+  }; 
+    
 
 
   services.gnome.gnome-keyring.enable = true;
