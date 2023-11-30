3e523f0c387fea9382786a5d950c413dd6bec3f6
Luca Gawalleck <luca.gawalleck@gmail.com>
2023-11-30 02:31:12 +0100

    set up luasnip

--- modules/nvim/nvim.lua
+++ modules/nvim/nvim.lua
@@ -96,3 +96,53 @@ cmp.setup {
         { name = 'luasnip' },
     },
 }
+
+-- luasnip.lua
+
+local ls = require "luasnip"
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local extras = require("luasnip.extras")
+local rep = extras.rep
+local fmt = require("luasnip.extras.fmt").fmt
+
+vim.keymap.set({"i", "s"}, "<A-k>", function ()
+		if ls.expand_or_jumpable() then
+			ls.expand_or_jump()
+		end
+end, { silent = true })
+
+vim.keymap.set({"i", "s"}, "A-j", function ()
+	if ls.jumpable(-1) then
+		ls.jump(-1)
+    end
+end, { silent = true })
+
+ls.add_snippets("tex", {
+		s("hello", {
+				t('print("hello '),
+				i(1),
+				t(' world")')
+		}),
+
+		s("if", {
+				t('if '),
+				i(1, "true"),
+				t(' then '),
+				i(2),
+				t(' end')
+		}),
+
+		ls.add_snippets("tex", {
+				s("beg", fmt(
+				[[ 
+				\begin{{{}}}
+						{}
+				\end{{{}}}
+				]], {
+						i(1), i(0), rep(1)
+				}))
+		})
+})
+
