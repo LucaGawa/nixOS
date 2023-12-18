819281c4fa13ed67e3fa259e1d0916af888fa062
Luca Gawalleck <luca.gawalleck@gmail.com>
2023-12-18 23:15:38 +0100

    add some mimeapp config

--- home.nix
+++ home.nix
@@ -128,6 +128,9 @@ programs.vscode = {
 	associations.added = {
 	"application/pdf" = ["org.gnome.Evince.desktop"];
 	"image/vnd.djvu+multipage"=["org.gnome.Evince.desktop"];
+	"image/jpeg=" ["org.nomacs.ImageLounge.desktop;"];
+	"image/png=" ["org.nomacs.ImageLounge.desktop;"];
+
 	};
 	defaultApplications = {
 	"application/pdf" = ["org.gnome.Evince.desktop"];
@@ -136,6 +139,8 @@ programs.vscode = {
     "x-scheme-handler/https" = ["firefox.desktop"];
     "x-scheme-handler/about" = ["firefox.desktop"];
     "x-scheme-handler/unknown" = ["firefox.desktop"];
+    "image/jpeg" = ["org.nomacs.ImageLounge.desktop"];
+    "image/png" = ["org.nomacs.ImageLounge.desktop"];
 	};
 	};
   # xdg.mimeApps.defaultApplications = {
