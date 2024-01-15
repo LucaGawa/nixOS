bbb4ce888368da6b8f723012f6fe2607a2235990
Luca Gawalleck <luca.gawalleck@gmail.com>
2024-01-15 15:58:41 +0100

    snippets again

--- modules/nvim/LuaSnip/tex/environments.lua
+++ modules/nvim/LuaSnip/tex/environments.lua
@@ -2,133 +2,72 @@ local helpers = require('luasnip-helper-funcs')
 local get_visual = helpers.get_visual
 local tex = require('luasnip-conditions')
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
-local function environments(trig,expr)
--- function for environment snippets like 
-return s({trig="" .. trig, wordTrig=false},
-  fmta(
-      [[\begin{]] .. expr .. [[*}
+local function environments(trig, expr)
+    -- function for environment snippets like
+    return s({trig = "" .. trig, wordTrig = false},
+             fmta([[\begin{]] .. expr .. [[*}
 				<>
-\end{]] .. expr .. [[*}]],
-    {d(1,get_visual)}
-),
-
-  	{condition = tex.line_begin }
-)
+\end{]] .. expr .. [[*}]], {d(1, get_visual)}), {condition = tex.line_begin})
 end
-local function lists(trig,expr)
-return s({trig="" .. trig, wordTrig=true},
-  fmta(
-      [[\begin{]] .. expr .. [[}
+local function lists(trig, expr)
+    return s({trig = "" .. trig, wordTrig = true},
+             fmta([[\begin{]] .. expr .. [[}
 				\item <>
-\end{]] .. expr .. [[}]],
-    {d(1,get_visual)}
-),
+\end{]] .. expr .. [[}]], {d(1, get_visual)}),
 
-  	{condition = tex.in_text and line_begin}
-)
+             {condition = tex.in_text and line_begin})
 end
-return{
-environments('eq','equation'),
-environments('ga','gather'),
-environments('al','align'),
-lists('it', 'itemize'),
--- lists('en', 'enumerate'),
+return {
+    environments('eq', 'equation'), environments('ga', 'gather'),
+    environments('al', 'align'), lists('it', 'itemize'),
+    -- lists('en', 'enumerate'),
 
-s({trig="dds", wordTrig=true, snippetType='autosnippet'},
-  fmta(
-      [[\begin{description}
+    s({trig = "dds", wordTrig = true, snippetType = 'autosnippet'},
+      fmta([[\begin{description}
 				\item[<>] <>
-\end{description}]],
-    {i(1), i(2)}
-),
+\end{description}]], {i(1), i(2)}), {condition = tex.in_text and line_begin}),
 
-  	{condition = tex.in_text and line_begin}
-),
-
-s({trig="en", wordTrig=true},
-  fmta(
-[[
+    s({trig = "en", wordTrig = true}, fmta([[
 \begin{enumerate}[label=(\arabic*)]
 				\item <>
 \end{enumerate}
-]],
-    {i(1)}
-),
+]], {i(1)}), {condition = tex.in_text and line_begin}),
 
-  	{condition = tex.in_text and line_begin}
-),
-
-s({trig="ii", wordTrig=true},
-				t("\\item"),
-				{condition = tex.in_text and tex.in_list and line_begin and not tex.in_description }
-),
-
-s({trig="mm", snippetType='autosnippet'},
-  fmta(
-     [[
+    s({trig = "ii", wordTrig = true}, t("\\item"), {
+        condition = tex.in_text and tex.in_list and line_begin and
+            not tex.in_description
+    }), s({trig = "mm", snippetType = 'autosnippet'}, fmta([[
        $<>$
-     ]],
-     { d(1, get_visual) }
-  )
-),
-s({trig="env", wordTrig=true, snippetType="autosnippet"},
-  fmta(
-    [[
+     ]], {d(1, get_visual)})), s({trig = "env", wordTrig = true}, fmta([[
       \begin{<>}
           <>
       \end{<>}
-    ]],
-    {
-      i(1),
-      i(2),
-      rep(1),  -- this node repeats insert node i(1)
-    }
-  )
-),
-s({trig="emm", wordTrig=true, snippetType="autosnippet"},
-  fmta(
-    [[
+    ]], {
+        i(1), i(2), rep(1) -- this node repeats insert node i(1)
+    }), {condition = line_begin}),
+    s({trig = "emm", wordTrig = true, snippetType = "autosnippet"}, fmta([[
       \begin{subequations}
        \begin{empheq}[box=\widefbox]{<>}
           <>
       \end{empheq}
        \end{subequations}
-    ]],
-    {
-      i(1),
-      i(2),
-    }
-  )
-),
-s({trig="cas", snippetType="autosnippet", wordTrig=true},
-  fmta(
-    [[
+    ]], {i(1), i(2)})),
+    s({trig = "cas", snippetType = "autosnippet", wordTrig = true}, fmta([[
       \begin{cases}
           <> & <> \\
-          <> & <> 
+          <> & <>
        \end{cases}
-    ]],
-    {
-      i(1),
-      i(2),
-      i(3),
-      i(4),
-    }
-  ),
-  { condition = tex.in_math }
-),
-s({trig="report"},
-  fmta(
-    [[\documentclass[english]{scrreprt}
+    ]], {i(1), i(2), i(3), i(4)}), {condition = tex.in_math}),
+    s({trig = "report"}, fmta([[\documentclass[english]{scrreprt}
     \usepackage{pakete}
       \begin{document}
           <>
       \end{document}
-    ]],
-    {
-      i(1),
-    }
-  )
-),
-
+    ]], {i(1)})), s({trig = "article"}, fmta(
+                        [[\documentclass[english]{scrartcl}
+    \usepackage{pakete}
+      \begin{document}
+          <>
+      \end{document}
+    ]], {i(1)}))
 }
--- modules/nvim/LuaSnip/tex/fonts.lua
+++ modules/nvim/LuaSnip/tex/fonts.lua
@@ -2,63 +2,30 @@ local helpers = require('luasnip-helper-funcs')
 local tex = require('luasnip-conditions')
 local get_visual = helpers.get_visual
 
-local function text(trig,expr)
--- function for text snippets like \textit{}
-				return s({trig="([^%a])" .. trig, wordTrig=false, regTrig=true, snippetType='autosnippet'},
-  fmta(
-      "<>\\" .. expr .. "{<>}",
-			{ f ( function(_, snip) return snip.captures[1] end),
-							d(1,get_visual)}
-        ),
-{ condition = tex.in_text }
-	)
+local function text(trig, expr)
+    -- function for text snippets like \textit{}
+    return s({trig = trig, wordTrig = true, snippetType = 'autosnippet'},
+             fmta("\\" .. expr .. "{<>}", {d(1, get_visual)}),
+             {condition = tex.in_text})
 end
 
-
-local function mathtext(trig,expr)
--- function for text snippets like \textit{}
-				return s({trig="" .. trig, wordTrig=true, snippetType='autosnippet'},
-  fmta(
-      "\\" .. expr .. "{<>}<>",
-			{d(1,get_visual),
-       i(0)}
-  ),
-{condition = tex.in_math}
-	)
+local function mathtext(trig, expr)
+    -- function for text snippets like \textit{}
+    return s({trig = "" .. trig, wordTrig = true, snippetType = 'autosnippet'},
+             fmta("\\" .. expr .. "{<>}<>", {d(1, get_visual), i(0)}),
+             {condition = tex.in_math})
 end
 
-return{
-     text('tt','texttt'),
-     text('tbb','textbf'),
-     text('tii','textit'),
-     text('tss','textsc'),
-     text('uu','underline'),
-     text('"','enquote'),
-     mathtext('tt', 'text'),
-     mathtext('it', 'intertext'),
-     mathtext('uu', 'uline'),
-     mathtext('du', 'uuline'),
-     mathtext('mr', 'mathrm'),
-     mathtext('mbb', 'mathbb'),
-     mathtext('du', 'uuline'),
-     mathtext('ov', 'overline'),
-s({trig="cal",wordTrig=false},
-  fmta(
-      "\\mathcal{<>}",
-			{i(1)}
-  )
-),
-s({trig="rm",wordTrig=false},
-  fmta(
-      "\\mathrm{<>}",
-			{i(1)}
-  )
-),
-s({trig="cb",wordTrig=false},
-  fmta(
-      "\\colorbox{yellow}{$<>$}",
-	 		{d(1, get_visual)}
-  )
-),
+return {
+    text('tt', 'texttt'), text('tbb', 'textbf'), text('tii', 'textit'),
+    text('tss', 'textsc'), text('uu', 'underline'), text('"', 'enquote'),
+    mathtext('tt', 'text'), mathtext('it', 'intertext'),
+    mathtext('uu', 'uline'), mathtext('du', 'uuline'), mathtext('mr', 'mathrm'),
+    mathtext('mbb', 'mathbb'), mathtext('du', 'uuline'),
+    mathtext('ov', 'overline'),
+    s({trig = "cal", wordTrig = false}, fmta("\\mathcal{<>}", {i(1)})),
+    s({trig = "rm", wordTrig = false}, fmta("\\mathrm{<>}", {i(1)})),
+    s({trig = "cb", wordTrig = false},
+      fmta("\\colorbox{yellow}{$<>$}", {d(1, get_visual)}))
 
- }
+}
