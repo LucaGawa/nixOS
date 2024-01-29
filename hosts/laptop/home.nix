8d4afd90b82d60508f17bf0ef39f29e0921ec644
Luca Gawalleck <luca.gawalleck@gmail.com>
2024-01-29 17:49:30 +0100

    update

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -1,19 +1,19 @@
 {
- wayland.windowManager.hyprland = {
-	settings = {
-		monitor = [
-			"eDP-1,2560x1600,2560x0,1.10"
-			"DP-4, 1920x1080, 0x0, 0.75"
-		];
-		bind = [
-			"$mainMod, z, exec, xournalpp"
-		];
-	};
-	};
-	
-programs.fish = {
-	shellAbbrs = {
-		rebuild = "sudo nixos-rebuild switch --flake ~/nixOS/#laptop";
-	};
+  wayland.windowManager.hyprland = {
+    settings = {
+      monitor = [
+        "eDP-1,2560x1600,2560x0,1"
+        "DP-4, 1920x1080, 0x0, 0.75"
+      ];
+      bind = [
+        "$mainMod, z, exec, xournalpp"
+      ];
+    };
+  };
+
+  programs.fish = {
+    shellAbbrs = {
+      rebuild = "sudo nixos-rebuild switch --flake ~/nixOS/#laptop";
+    };
   };
 }
