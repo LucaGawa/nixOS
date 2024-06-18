f5a11e8424e7b5d8b0291951b203e9f6a02fd335
Luca Gawalleck <luca.gawalleck@gmail.com>
2024-06-18 22:37:18 +0200

    update

--- hosts/mac/configuration.nix
+++ hosts/mac/configuration.nix
@@ -2,20 +2,72 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ 
-                pkgs.neovim
+        with pkgs; [ 
+                neovim
+                starship
+                zoxide
+                eza
+                bat
+                atuin
         ];
+
+programs.bash = {
+  interactiveShellInit = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    then
+      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+  '';
+};
+
+      
+  stylix = {
+    enable = false;
+    image = /Users/luca/nixOS/wallpaper/1.png;
+  #   base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+  #  # cursor = {
+  #   #   package = pkgs.nordzy-cursor-theme;
+  #   #   name = "Nordzy-cursors";
+  #   # };
+  #   fonts = {
+  #     monospace = {
+  #       package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
+  #       name = "FiraCode Nerd Font Mono";
+  #     };
+  #     sansSerif = {
+  #       package = pkgs.fira;
+  #       name = "Fira Sans Medium";
+  #     };
+  #     serif = {
+  #       package = pkgs.dejavu_fonts;
+  #       name = "DejaVu Serif";
+  #     };
+  #     sizes = {
+  #       applications = 10;
+  #       terminal = 14;
+  #       desktop = 8;
+  #       popups = 10;
+  #     };
+  #   };
+    # opacity = {
+    #   applications = 1.0;
+    #   terminal = 0.9;
+    #   desktop = 0.0;
+    #   popups = 1.0;
+    # };
+  };
 
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+      nix.package = pkgs.nix;
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
       # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
+      # programs.zsh.enable = true;  # default shell on catalina
+      programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
--- hosts/mac/flake.nix
+++ hosts/mac/flake.nix
@@ -7,9 +7,10 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
         home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    stylix.url = "github:danth/stylix";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, stylix, ... }:
   let
 	system = "x86_64-darwin";
 	userSet = {
@@ -20,11 +21,13 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#luca
     darwinConfigurations."luca" = nix-darwin.lib.darwinSystem {
+      inherit system;
       specialArgs = {
           inherit inputs;
 	  inherit userSet;
       };
       modules = [ ./configuration.nix
+      stylix.darwinModules.stylix
 	home-manager.darwinModules.home-manager
 {
             home-manager.useGlobalPkgs = true;
