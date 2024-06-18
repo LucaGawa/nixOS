1e49fd36bdb8a676b9ab79a63598a7a2af4df71c
Luca Gawalleck <luca.gawalleck@gmail.com>
2024-06-18 22:44:46 +0200

    added packages

--- hosts/mac/configuration.nix
+++ hosts/mac/configuration.nix
@@ -1,31 +1,39 @@
-{ pkgs, inputs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-                pkgs.neovim
-        ];
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  imports = [
+    ../../system/programs_basic.nix
+    ../../system/python.nix
+  ];
+  # environment.systemPackages =
+  #   [
+  #           pkgs.neovim
+  #   ];
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = false; # default shell on catalina
+  # programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "x86_64-darwin";
 
-      nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 }
