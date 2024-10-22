edfdae21ceedce3a583902c2445325fda8dc9e2b
Luca Gawalleck <luca.gawalleck@gmail.com>
2024-10-22 14:03:38 +0200

    temp

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -18,9 +18,10 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "eDP-1,2560x1600@240,2560x438,1.6,"
+        "eDP-1,2560x1600@240,4000x1397,1.6,"
         # "DP-4, 3840x2160, 0x0, 1.5"
-        "DP-6, 3840x2160, 0x0, 1.5"
+        # "DP-5, 3840x2160, 0x0, 1.5, transform, 1"
+        # "HDMI-A-1, 3840x2160, 1440x958, 1.5"
       ];
       bind = [
         "$mainMod, z, exec, xournalpp"
